411ee058d7236b8cd11e19defaaa6c79f470b5d9
Tan Jing Yu, Denis <denis.tanjingyu@Hotmail.sg>
2020-08-14 04:20:32 +0800

    Update PLC1.m

--- PLC1.m
+++ PLC1.m
@@ -4,6 +4,7 @@ P1O1_t_1 = PLC1(2);
 LIT101_t_1 = PLC1(3);
 FIT101_t_1 = PLC1(4);
 FIT201_t_1 = PLC1(5);
+
 %%----update current state------
 LIT101_t = LIT101_t_1 + (FIT101_t_1-FIT201_t_1) * 5 / AreaTank1 + PLC6 one parameters
 if (LIT101_t > HLimit)
