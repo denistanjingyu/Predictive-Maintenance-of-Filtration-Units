6f12f3c9c5c63dbd13ae781c2f18171d19cd7015
Tan Jing Yu, Denis 陈劲宇 <denis.tanjingyu@Hotmail.sg>
2020-10-02 14:59:35 +0800

    Update PLC5.m

--- PLC5.m
+++ PLC5.m
@@ -33,56 +33,56 @@ if p5_state = 4:  % low pressure flushing
 end if
 
 if p5_state = 5:  % starts high pressure pump (HPP), either P501 or P502
-  if P501_speed > pump_auto_speed | P502_speed > pump_auto_speed :  % pump speed has to be more than its automated speet / default speed
+  if P501_speed > pump_auto_speed | P502_speed > pump_auto_speed:  % pump speed has to be more than its automated speet / default speed
     p5_state = 6
   end if
 end if
 
 if p5_state = 6:  % maintain at least 10% of FIT501 flow. ramps 4.5 per second of final flow until desired flow is reached
-  if pump_auto_speed < vsd_high_speed :
-    if PIT502 < PIT503 && PIT501<275 :
+  if pump_auto_speed < vsd_high_speed:
+    if PIT502 < PIT503 && PIT501<275:
       pump_auto_speed = pump_auto_speed + 0.5
     end if
-    if FIT501 > HMI_HPP_Q_SET_M3H && PIT502<PIT503 && PIT501.>260 : % HMI_HPP_Q_SET_M3H is preset to be 0.6 (see above)
+    if FIT501 > HMI_HPP_Q_SET_M3H && PIT502<PIT503 && PIT501.>260: % HMI_HPP_Q_SET_M3H is preset to be 0.6 (see above)
       p5_state = 7
     end if
   end if
 end if
 
-if p5_state = 7 : % checks if conductivity > conductivity setpoint
-  if AIT504 < AIT504_SAH :  % which one is the conductivity setpoint, AIT504 or AIT504.SAH
+if p5_state = 7: % checks if conductivity > conductivity setpoint
+  if AIT504 < AIT504_SAH:  % which one is the conductivity setpoint, AIT504 or AIT504.SAH
     p5_state = 8
   end if
 end if
 
 % for MVxxx, 1 means closed, 2 means open, 0 means in-between
 
-if p5_state = 8 : % opens MV501
-  if MV501 = 2 :
+if p5_state = 8: % opens MV501
+  if MV501 = 2:
     p5_state = 9
   end if
 end if
 
-if p5_state = 9 : % closes MV503
+if p5_state = 9: % closes MV503
   if MV503 = 1
     p5_state = 10
   end if
 end if
 
-if p5_state = 10 : % opens MV502
+if p5_state = 10: % opens MV502
   if MV502 = 2
     p5_state = 11
   end if
 end if
 
-if p5_state = 11 : % closes MV504
+if p5_state = 11: % closes MV504
   if MV504 = 1
     p5_state = 12
   end if
 end if
 
-if p5_state = 12 :  % RO permeate production
-  if plant = 0 :    % if plant stops
+if p5_state = 12:  % RO permeate production
+  if plant = 0:    % if plant stops
     p5_state = 13
   end if
 end if
@@ -91,62 +91,62 @@ if p5_state = 13:   % ramp down / decrease VSD ;
   if P501_speed > vsd_min_speed : % vsd_mean_speed = 10, preset
     pump_auto_speed = pump_auto_speed + 0.5
   end if
-  if P501_speed < vsd_min_speed :
+  if P501_speed < vsd_min_speed:
     p5_state = 14
   end if
 end if
 
-if p5_state = 14 :  % stops high-pressure pump
-  if RO_high = 0  :  % 0 means not running, 1 means running
+if p5_state = 14:  % stops high-pressure pump
+  if RO_high = 0 :  % 0 means not running, 1 means running
     p5_state = 15
   end if
 end if
 
-if p5_state = 15 :  % opens MV504  (automatic when it reaches timeout of 120 seconds)
+if p5_state = 15:  % opens MV504  (automatic when it reaches timeout of 120 seconds)
   MV504_timeout = MV504_timeout + 1
-  if MV504 = 2 | MV504_timeout >120  
+  if MV504 = 2 | MV504_timeout > 120  
     p5_state = 16
   end if
 end if
 
-if p5_state = 16 :  % opens MV502  (automatic when it reaches timeout of 120 seconds)
+if p5_state = 16:  % opens MV502  (automatic when it reaches timeout of 120 seconds)
   MV502_timeout = MV502_timeout + 1
-  if MV502 = 1 | MV502_timeout >120 :
+  if MV502 = 1 | MV502_timeout >120:
     p5_state = 17
   end if
 end if
 
-if p5_state = 17 :  % opens MV503 (automatic when it reaches timeout of 120 seconds)
+if p5_state = 17:  % opens MV503 (automatic when it reaches timeout of 120 seconds)
   MV503_timeout = MV503_timeout + 1
-  if MV503 = 2 | MV503_timeout >120 :
+  if MV503 = 2 | MV503_timeout >120:
     p5_state = 18
   end if
 end if
 
-if p5_state = 18 :  % opens MV501 (automatic when it reaches timeout of 120 seconds)
+if p5_state = 18:  % opens MV501 (automatic when it reaches timeout of 120 seconds)
   MV501_timeout = MV501_timeout + 1
-  if MV501 = 2 | MV501_timeout > 120 :
+  if MV501 = 2 | MV501_timeout > 120:
     p5_state = 19
   end if
 end if
 
-if p5_state = 19 :  % keeps RO feed pumping
+if p5_state = 19:  % keeps RO feed pumping
   RO_flushing_minute = RO_flushing_minute + 1
-  if RO_flushing_minute = RO_flushing_minute_sp : % WHAT IS flushing_minute_sp
+  if RO_flushing_minute = RO_flushing_minute_sp: % WHAT IS flushing_minute_sp
     p5_state = 20
   end if
 end if
 
-if p5_state = 20 :  % shuts down RO feed pump
-  if RO_feed = 0 :
+if p5_state = 20:  % shuts down RO feed pump
+  if RO_feed = 0:
     p5_state = 21
   end if
 end if
 
-if p5_state = 21 :  % closes MV503 and MV504
+if p5_state = 21:  % closes MV503 and MV504
   MV503_timeout = MV503_timeout + 1
   MV504_timeout = MV504_timeout + 1
-  if (MV503 = 1 && MV504 = 1) | (MV503_timeout > 120 && MV504_timeout > 120) :
+  if (MV503 = 1 && MV504 = 1) | (MV503_timeout > 120 && MV504_timeout > 120):
     p5_state = 1
   end if
 end if
