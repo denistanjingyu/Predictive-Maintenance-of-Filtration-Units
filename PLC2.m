0a3840059828c55431a5c5d0645258b58fc68d87
Tan Jing Yu, Denis 陈劲宇 <denis.tanjingyu@Hotmail.sg>
2020-10-03 03:04:21 +0800

    Update PLC2.m

--- PLC2.m
+++ PLC2.m
@@ -1,18 +1,18 @@
 load('PLC2.mat')
 
-%AIT201_t_1 = ;
-%AIT202_t_1 = ;
-%AIT203_t_1 = ;
+% AIT201_t_1 = ;
+% AIT202_t_1 = ;
+% AIT203_t_1 = ;
 MV201_t_1 = 0;
 P201_t_1 = 0;
 P203_t_1 = 0;
 P205_t_1 = 0;
 P207_t_1 = 0;
-%T201_t_1 = ;
-%T202_t_1 = ;
-%T203_t_1 = ;
+% T201_t_1 = ;
+% T202_t_1 = ;
+% T203_t_1 = ;
 
-%%---judge situation this PLC-------
+%% ---judge situation this PLC-------
 if MV201_t_1 == 1
     P201_t_1 = 1;
     P203_t_1 = 1;
